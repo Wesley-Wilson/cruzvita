31165d807731d8c459e871c6f4f016846696606b
Wesley-Wilson <wesleymkxbeta@gmail.com>
2023-02-09 17:22:20 -0300

    primeiro commit

---
+++ src/main/java/br/com/cruzvita/Banco.java
@@ -0,0 +1,14 @@
+package br.com.cruzvita;
+
+import java.util.Scanner;
+
+public class Banco {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String nome = "Wesley Wilson";
+		
+		System.out.println(nome);
+		
+		sc.close();
+	}
+}
